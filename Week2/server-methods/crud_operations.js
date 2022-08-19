f9a360f2307d6551dc852541949dfab0b6c11b26
Sheriff Adekunle Tajudeen <dnexteinstein2@gmail.com>
2022-08-19 12:58:37 +0100

    Crud Operations:
    Finished feature to get all books from the record.
    Finished feature to add a new book to the record.

--- Week2/server-methods/crud_operations.js
+++ Week2/server-methods/crud_operations.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-// const fs = require('fs');
-// const path = require('path');
 
 const booksDbPath = path.join(__dirname, 'db', 'books.json');
 
@@ -11,30 +9,58 @@ function getAllBooks(req, res) {
     res.writeHead(200);
     fs.readFile(booksDbPath, 'utf8', (err, data) => {
         if (err) {
-            console.log(`An error occured: ${err}`);
+            fileError(err, res);
         }
+        res.writeHead(200);
         res.end(data);
     });
 }
 
 
 function addBook(req, res) {
-    fs.readFile(path.join(__dirname, 'db', 'books.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.log(`An error occured: ${err}`);
-        }
-        const booksDataStream = []
-        req.on("data", (chunk) => {
-            booksDataStream.push(chunk);
-        });
-        req.on("end", () => {
-            res.writeHead(200);
-            const allBooks = Buffer.concat(booksDataStream).toString();
-            console.log(allBooks);
-        })
+    const booksDataStream = [];
+    req.on("data", (chunk) => {
+        booksDataStream.push(chunk);
+    });
+
+    req.on("end", () => {
+        fs.readFile(booksDbPath, 'utf8', (err, data) => {
+            if (err) {
+                fileError(err, res);
+            }
+            
+            let oldBooks = data;
+            let newBook = JSON.parse(booksDataStream);
+            oldBooks = JSON.parse(oldBooks);
+
+            //Generate id for new book
+            let newBookId = oldBooks[oldBooks.length - 1].id + 1;
+
+            //Append new data to exiting record
+            newBook.id = newBookId;
+            let updatedBooks = [...oldBooks, newBook];
+
+            //Write updated record to file
+            updatedBooks = JSON.stringify(updatedBooks);
+            fs.writeFile(booksDbPath, updatedBooks, (err) => {
+                if (err) {
+                    fileError(err, res);
+                }
+                res.end('Success, new book has been added!');
+            });
+        });        
     });
 }
 
+
+// function updateBook()
+
+
+function fileError(err, res) {
+    res.writeHead(400);
+    console.log(`An error occured: ${err}`);
+    res.end('Failed, try again!');
+}
 
 
 module.exports = {
